9b9bd3c9c939f74731b099031c4e8d8cbb204f30
Tom Raaff <tomraaff@protonmail.com>
2025-04-01 21:34:13 +0200

    hide todos in production

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
 console.log("app.js is loaded");
+
+if (window.location.hostname !== "localhost") {
+  const todos = document.querySelectorAll(".todo");
+  todos.forEach((element) => (element.style.display = "none"));
+}
